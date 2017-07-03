622fe04ec0af97cdad210dbb4cbbc464cd79a7b1
James Corder Guy <corder.guy@gmail.com>
2017-07-02 19:55:05 -0500

    Removed std::bind for ~20% speedup

---
+++ test.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+for ((i=0;i<10;i=i+1)) do
+    time ./triangle 50000000
+done
--- triangle.cpp
+++ triangle.cpp
@@ -1,11 +1,10 @@
-#include <functional>
 #include <png++/png.hpp>
 #include <random>
 
 #define IMAGE_SIZE 1024
 
-int avg(int a, int b) {
-    return (a + b) / 2;
+void avg(int &a, int b) {
+    a = (a + b) / 2;
 }
 
 int main(int argc, char* argv[]) {
@@ -20,23 +19,22 @@ int main(int argc, char* argv[]) {
 
     png::image<png::rgb_pixel> image(IMAGE_SIZE, IMAGE_SIZE);
 
-    auto flip = std::bind(distribution, generator);
     int x = IMAGE_SIZE;
     int y = IMAGE_SIZE;
 
     for (int i = 0; i < iterations; i++) {
-        switch (flip()) {
+        switch (distribution(generator)) {
         case 0:
-            x = avg(x, IMAGE_SIZE / 2);
-            y = avg(y, 0);
+            avg(x, IMAGE_SIZE / 2);
+            avg(y, 0);
             break;
         case 1:
-            x = avg(x, 0);
-            y = avg(y, IMAGE_SIZE);
+            avg(x, 0);
+            avg(y, IMAGE_SIZE);
             break;
         case 2:
-            x = avg(x, IMAGE_SIZE);
-            y = avg(y, IMAGE_SIZE);
+            avg(x, IMAGE_SIZE);
+            avg(y, IMAGE_SIZE);
             break;
         }
 
