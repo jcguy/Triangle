fb1153c637bb5b08a90045e787e2c6d536932a7e
James Corder Guy <corder.guy@gmail.com>
2018-02-01 14:17:17 -0600

    Update

--- triangle.cpp
+++ triangle.cpp
@@ -1,28 +1,49 @@
+/*
+ * Sierpenski Triangle generator
+ * Uses a random walk to generate the fractal
+ *
+ * James Corder Guy
+ */
+
 #include <png++/png.hpp>
 #include <random>
 
+// The output is a square; this is the length of one side
 #define IMAGE_SIZE 1024
 
+// The number of points to generate. Can be overridden by a command line arg
+#define ITERATIONS 1024
+
+// The output filename
+#define OUTFILE "image.png"
+
+// Sets the first argument to the average of it and the second
 void avg(int &a, int b) {
     a = (a + b) / 2;
 }
 
 int main(int argc, char* argv[]) {
-    int iterations = 1000;
-
+    // Override the number of iterations if necessary
+    int iterations = ITERATIONS;
     if (argc == 2) {
         iterations = atoi(argv[1]);
     }
 
+    // Initialize the random number generator
     std::default_random_engine generator;
     std::uniform_int_distribution<int> distribution(0, 2);
 
+    // Initialize the output image (the default is solid black)
     png::image<png::rgb_pixel> image(IMAGE_SIZE, IMAGE_SIZE);
 
+    // Start the random walk in the lower-right corner of the image
     int x = IMAGE_SIZE;
     int y = IMAGE_SIZE;
 
+    // The random walk
     for (int i = 0; i < iterations; i++) {
+        // Depending on which of the three outputs of the generator is
+        // produced, move the point halfway to one of the corner points
         switch (distribution(generator)) {
         case 0:
             avg(x, IMAGE_SIZE / 2);
@@ -38,8 +59,10 @@ int main(int argc, char* argv[]) {
             break;
         }
 
+        // Set the pixel at that point to white
         image.set_pixel(x, y, png::rgb_pixel(255, 255, 255));
     }
 
-    image.write("output.png");
+    // Write to image to file
+    image.write(OUTFILE);
 }
